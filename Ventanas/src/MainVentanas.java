e9021e7911468bec31aa7c2288dc3029bc16046e
Manuela García <mxg2548@gmail.com>
2025-07-10 19:01:56 -0500

    Ejercicio de Ventanas Main

---
+++ Ventanas/src/MainVentanas.java
@@ -0,0 +1,12 @@
+public class MainVentanas {
+
+    public static void main(String[] args) {
+        //Ventanas
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+
+        VentanaSecundaria ven2 = new VentanaSecundaria();
+        ven2.setVisible(true);
+
+    }
+}
